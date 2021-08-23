54722f90775ac8688f537b1a77726dd69daefed4
João Bispo <joaobispo2077@gmail.com>
2021-08-23 17:20:29 -0300

    feat: setup nodemon

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ const app = express();
 
 const PORT = 3333;
 
+app.get('/api', (req, res) => res.json({ message: 'hello world' }));
+
 app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
