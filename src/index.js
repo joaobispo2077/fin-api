de5ae9202fb4b1291e27743241b873b84187f636
João Bispo <joaobispo2077@gmail.com>
2021-08-23 17:36:36 -0300

    feat: add minimal controllers example

--- src/index.js
+++ src/index.js
@@ -6,4 +6,13 @@ const PORT = 3333;
 
 app.get('/api', (req, res) => res.json({ message: 'hello world' }));
 
+app.get('/courses', (req, res) =>
+  res.json(['Análise e Desenvolvimento de Sistemas', 'Medicina', 'Recursos Humanos']));
+app.post('/courses', (req, res) => res.status(201).json());
+
+app.get('/courses/:id', (req, res) => res.status(201).json({ name: 'Medicina' }));
+app.put('/courses/:id', (req, res) => res.status(206).json());
+app.patch('/courses/:id', (req, res) => res.status(204).json());
+app.delete('/courses/:id', (req, res) => res.status(204).json());
+
 app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
